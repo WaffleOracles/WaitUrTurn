47670e97c7bab911bbe2f80d004e993d61f496f4
Fromsa Hera <fromsahera28@gmail.com>
2020-02-03 17:16:20 +0100

    add the request to a list called Serving, remove it from the Question

--- public/javascripts/scriptansatt.js
+++ public/javascripts/scriptansatt.js
@@ -7,61 +7,83 @@ function QueueSlot(navnIn, fagIn, bordNummerIn) {
 }
 
 var queue = [];
+var serving = [];
+nrQueueItems = 0;
 
 window.onload = function() {
     var socket = new WebSocket("ws://localhost:4000");
     socket.onmessage = function(event) {
       processSlotFromServer(event.data);
-    }
+    };
     socket.onopen = function() {
         socket.send("REQ QUEUE"); 
     }
 };
 
 
-function processSlotFromServer(data) {
-  const processedData = JSON.parse(data);
-  queue.push(new QueueSlot(processedData.navn, processedData.fag, processedData.bordnummer));
-  console.log('this is called');
+function createQueueItem(queueSlot, number) {
+  let queueItem = document.createElement('div');
+  queueItem.className = 'queueItem';
+  queueItem.setAttribute('id', String(number));
+
+  let queueItemText = document.createElement('div');
+  queueItemText.className = 'queueItemText';
+
+  let itemTextNavn = document.createElement('p');
+  let itemTextFag = document.createElement('p');
+  let itemTextBord = document.createElement('p');
+  itemTextNavn.className = 'itemText';
+  itemTextFag.className = 'itemText';
+  itemTextBord.className = 'itemText';
+  itemTextNavn.innerHTML = 'Navn: ' + queueSlot.navn;
+  itemTextFag.innerHTML = 'Fag: ' + queueSlot.fag;
+  itemTextBord.innerHTML = 'Bord nummer: ' + queueSlot.bordNummer;
+
+  queueItemText.appendChild(itemTextNavn);
+  queueItemText.appendChild(itemTextFag);
+  queueItemText.appendChild(itemTextBord);
+  queueItem.appendChild(queueItemText);
+
+  let queueItemButton = document.createElement('div');
+  queueItemButton.className = 'queueItemButton';
+  queueItemButton.setAttribute('onclick','acceptRequest(this)');
+
+  let itemTextButton = document.createElement('p');
+  itemTextButton.className = 'itemText';
+  itemTextButton.innerHTML = 'Accept';
 
 
-  let queueitem = document.createElement('div');
-  queueitem.className = 'queueitem';
-
-  let queueitemtext = document.createElement('div');
-  queueitemtext.className = 'queueitemtext';
-
-  let itemtextnavn = document.createElement('p');
-  let itemtextfag = document.createElement('p');
-  let itemtextbord = document.createElement('p');
-  itemtextnavn.className = 'itemtext';
-  itemtextfag.className = 'itemtext';
-  itemtextbord.className = 'itemtext';
-  itemtextnavn.innerHTML = 'Navn: ' + processedData.navn;
-  itemtextfag.innerHTML = 'Fag: ' + processedData.fag;
-  itemtextbord.innerHTML = 'Bord nummer: ' + processedData.bordnummer;
-
-  queueitemtext.appendChild(itemtextnavn);
-  queueitemtext.appendChild(itemtextfag);
-  queueitemtext.appendChild(itemtextbord);
-  queueitem.appendChild(queueitemtext);
-
-  let queueitembutton = document.createElement('div');
-  queueitembutton.className = 'queueitembutton';
-  
-  let itemtextButton = document.createElement('p');
-  itemtextButton.className = 'itemtext';
-  itemtextButton.innerHTML = 'Accept';
-
-  queueitembutton.appendChild(itemtextButton);
-  queueitem.appendChild(queueitembutton);
-
-  // Finally add the new element to the queue section
-  let queueSection = document.getElementsByClassName('queuesection');
-  queueSection[0].appendChild(queueitem);
+  queueItemButton.appendChild(itemTextButton);
+  queueItem.appendChild(queueItemButton);
+  return queueItem;
 }
 
+function processSlotFromServer(data) {
+  const processedData = JSON.parse(data);
+  let queueSlot = new QueueSlot(processedData.navn, processedData.fag, processedData.bordnummer)
+  queue.push(queueSlot);
+  console.log('this is called');
 
+  let queueItem = createQueueItem(queueSlot, nrQueueItems++);
+
+  // Finally add the new element to the queue section
+  let queueSection = document.getElementsByClassName('queueSection');
+  queueSection[0].appendChild(queueItem);
+}
+
+function acceptRequest(id){
+  //console.clear();
+  let next = id.parentElement;
+  let serving = document.getElementsByClassName('serving')[0];
+  serving.appendChild(next);
+  next.children[1].children[0].innerHTML = 'Done';
+  next.children[1].setAttribute('onclick','doneRequest(this)')
+}
+
+function doneRequest(id) {
+  let done = id.parentElement;
+  done.remove();
+}
 
 
 
